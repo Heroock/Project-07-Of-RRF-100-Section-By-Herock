ff25b27dc778c93b0b758306e26aa93f2b706417
MH Herock <m.h.herock@gmail.com>
2017-05-17 01:58:25 +0600

    RRF Awesome Creation !!!

---
+++ js/active.owl.carousel.js
@@ -0,0 +1,26 @@
+/* ===============================================================================
+ * Name: JS Style V3.0 || 2017 !!!
+ * Company: Banglasoft@Corporation™ !!!
+ * Founder: Engr. Md Mahadi Hassan Herock !!!
+ * Website URL-I: http://www.banglasoft.net !!!
+ * Website URL-II: http://www.bangla-soft.com !!!
+ * Description: Try To Do Something Better Than Other !!!
+ ============================================================================== */
+
+/* ====================== JS Plugins Start From Here ========================== */
+
+/* ================ Owl Carousel JS Plugin Start From Here ==================== */
+
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel({
+		items: 4,
+		loop: true,
+		margin: 40,
+		nav: true,
+		navText: ["<i class='fa fa-hand-o-left'></i>", "<i class='fa fa-hand-o-right'></i>"]
+	});
+});
+
+/* ==================== Owl Carousel JS Plugin End Here ======================= */
+
+/* ========================= JS Plugins End Here ============================== */
